3a378dc16f2f339224b2c9f8707a9c03ed263ea2
MehdiSaffar <saffar.mehdi3@gmail.com>
2018-01-26 21:44:56 +0300

    Started working on LineHelper editor

--- Assets/Editor/Climbing/LineHelperInspector.cs
+++ Assets/Editor/Climbing/LineHelperInspector.cs
@@ -1,15 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 
 using UnityEditor;
 
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 [CustomEditor(typeof(PointsHelperLine))]
 public class LineHelperInspector : Editor{
 	private PointsHelperLine line;
 	private Transform handleTransform;
 	private Quaternion handleRotation;
+	/// <summary>
+	/// Normal of the line
+	/// </summary>
+	private Vector3 norm;
+	/// <summary>
+	/// Color of the normal vector
+	/// </summary>
+	private readonly Color normalColor = Color.black;
+	/// <summary>
+	/// Color of the line
+	/// </summary>
+	private readonly Color lineColor = Color.gray;
+	/// <summary>
+	/// Midpoint of the line
+	/// </summary>
+	private Vector3 midpoint;
+
+	/// <summary>
+	/// Snap angle of the normal rotation in degrees
+	/// </summary>
+	private const float normalSnapAngle = 22.5F;
+
+	/// <summary>
+	/// Display length of the normal vector (in world space)
+	/// </summary>
+	private const float normalVectorLength = 0.5f;
+	/// <summary>
+	/// Display radius of the normal disk (in world space)
+	/// </summary>
+	private const float normalDiskRadius = normalVectorLength / 4f;
+
 	private void OnSceneGUI(){
 		// Get the target script
 		line = (PointsHelperLine) target;
@@ -20,15 +54,78 @@ public class LineHelperInspector : Editor{
 		// Set the rotation to local or global based on the user choice
 		handleRotation = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
 
-		var start = line.startPosition;
-		var end = line.endPosition;
-		ShowPoint(start);
-		ShowPoint(end);
+		// Show the point and get the new positions if changed
+		var p0 = ShowPoint(0);
+		var p1 = ShowPoint(1);
 
+		// Get the nomal of the line
+		norm = line.GetNormal();
+		midpoint = Vector3.Lerp(p0, p1, 0.5f);
+		ShowNormal();
+
+		Handles.color = lineColor;
+		Handles.DrawLine(p0, p1);
 	}
 
-	private void ShowPoint(Transform point){
-		var p = point.position;
-
+	public override void OnInspectorGUI(){
+		DrawDefaultInspector();
+		line = (PointsHelperLine) target;
+		EditorGUI.BeginChangeCheck();
+		var shouldReset = GUILayout.Button("Reset");
+		if(EditorGUI.EndChangeCheck()) {
+			if (shouldReset) {
+				Undo.RecordObject(line, "Reset line");
+				EditorUtility.SetDirty(line);
+				line.Reset();
+			}
+		}
 	}
+
+	/// <summary>
+	/// Shows the point, changes its position if done by the user and returns
+	/// its new position in both cases
+	/// </summary>
+	/// <param name="index">Index of the point in the line</param>
+	/// <returns>The current point position (world space)</returns>
+	private Vector3 ShowPoint(int index){
+		var pos = handleTransform.TransformPoint(line.GetPoint(index));
+		EditorGUI.BeginChangeCheck();
+		pos = Handles.DoPositionHandle(pos, handleRotation);
+		if(EditorGUI.EndChangeCheck())
+		{
+			Undo.RecordObject(line, "Move point");
+			EditorUtility.SetDirty(line);
+			line.SetPoint(index, handleTransform.InverseTransformPoint(pos));
+		}
+		return pos;
+	}
+
+	/// <summary>
+	/// Shows the normal
+	/// </summary>
+	/// <returns>New normal vector</returns>
+	private Vector3 ShowNormal(){
+		// Draw the normal
+		var worldNormal = handleTransform.TransformVector(norm);
+		Handles.DrawLine(midpoint, midpoint + worldNormal * normalVectorLength);
+
+		var normalRotation = Quaternion.LookRotation(worldNormal, line.GetDirectionVector());
+		EditorGUI.BeginChangeCheck();
+		var newNormalRotation = Handles.Disc(normalRotation,
+			midpoint,
+			line.GetDirectionVector(),
+			normalDiskRadius,
+			false,
+			normalSnapAngle);
+		if(EditorGUI.EndChangeCheck())
+		{
+			Undo.RecordObject(line, "Change line normal");
+			EditorUtility.SetDirty(line);
+			var newWorldNormal = newNormalRotation * Quaternion.Inverse(normalRotation) * worldNormal;
+			var newLocalNormal = handleTransform.InverseTransformVector(newWorldNormal);
+			line.SetNormal(newLocalNormal);
+		}
+		return Vector3.zero;
+	}
+
 }
--- Assets/Scripts/Climbing/PointsHelperLine.cs
+++ Assets/Scripts/Climbing/PointsHelperLine.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+using UnityEditor;
 
 using UnityEngine;
 
 /// <summary>
-/// Helper class to quickly set climb points along a line defined from <see cref="startPosition"/> to <see cref="endPosition"/>
+/// Helper class to quickly set climb points along a line defined from <see cref="p0"/> to <see cref="p1"/>
 /// </summary>
 [ExecuteInEditMode]
 public class PointsHelperLine : MyMonoBehaviour
@@ -16,76 +20,139 @@ public class PointsHelperLine : MyMonoBehaviour
 	/// Point prefab to be used for placement
 	/// </summary>
 	[SerializeField] private Point pointPrefab;
+
+	public Vector3[] points;
+
 	/// <summary>
-	/// Start position of climb points
+	/// Normal of the line
 	/// </summary>
-	public Transform startPosition;
-	/// <summary>
-	/// End position of climb points
-	/// </summary>
-	public Transform endPosition;
+	public Vector3 normal;
 
 	/// <summary>
 	/// Distance between points
 	/// </summary>
-	[SerializeField] [UnityEngine.Range(0.1f,3f)] private float intervalDistance;
-
-	/// <summary>
-	/// Length of the gizmo ray that shows the normal of the points
-	/// </summary>
-	[SerializeField] private float normalRayLength;
+	[SerializeField] [Range(0.1f,3f)] private float intervalDistance;
 
 	[SerializeField] private List<Point> placedPoints;
 
-	[SerializeField] private bool update;
-
 	private void Awake(){
 		placedPoints = new List<Point>();
-		update = false;
+		Reset();
 	}
 
-	private void Update(){
-		if (update) {
-			update = false;
-			RemovePointsFromScene();
-			SetPointsInScene();
-		}
+	/// <summary>
+	///  Resets the line
+	/// </summary>
+	public void Reset(){
+		points = new[]{
+			Vector3.left,
+			Vector3.right,
+		};
+		normal = Vector3.forward;
 	}
 
-	private void RemovePointsFromScene(){
-		foreach (var placedPoint in placedPoints) {
-			if(placedPoint)
-				DestroyImmediate(placedPoint.gameObject);
+//	private void Update(){
+//		if (update) {
+//			update = false;
+//			RemovePointsFromScene();
+//			SetPointsInScene();
+//		}
+//	}
 
-		}
-		placedPoints.Clear();
+//	private void RemovePointsFromScene(){
+//		foreach (var placedPoint in placedPoints) {
+//			if(placedPoint)
+//				DestroyImmediate(placedPoint.gameObject);
+//
+//		}
+//		placedPoints.Clear();
+//	}
+
+//	private void SetPointsInScene(){
+//		Point firstPoint = null;
+//		var totalDistance = (endPosition.transform.position - startPosition.transform.position).magnitude;
+//		var pointsCount = totalDistance / intervalDistance;
+//		for (int i = 0; i < pointsCount; i++) {
+//			var position = Vector3.Lerp(startPosition.position, endPosition.position, i / pointsCount);
+//			var newPoint = Instantiate(pointPrefab.gameObject, position, transform.rotation, pointsList)
+//				.GetComponent<Point>();
+//			newPoint._pointsList = pointsList;
+//			if (i == 0) {
+//				firstPoint = newPoint;
+//			}
+//			placedPoints.Add(newPoint);
+//		}
+//
+//		if(firstPoint)
+//			firstPoint._isRoot = true;
+//	}
+
+//	private void OnDrawGizmos()
+//	{
+//		Gizmos.color = Color.black;
+//		Gizmos.DrawLine(startPosition.position, endPosition.position);
+//
+//		var midline = Vector3.Lerp(startPosition.position, endPosition.position, 0.5f);
+//		GizmosUtil.DrawArrow(midline, midline + transform.forward * normalRayLength);
+//	}
+
+	/// <summary>
+	/// Gets the point with the specified index
+	/// </summary>
+	/// <param name="index">Index of the point in the line</param>
+	/// <returns>The coordinates of the point in line local space</returns>
+	public Vector3 GetPoint(int index) => points[index];
+
+	/// <summary>
+	/// Returns the line  vector from point at <paramref name="startIndex"/>, to point at <paramref name="endIndex"/>
+	/// Returns the vector of the first line segment by default
+	/// </summary>
+	/// <param name="startIndex">Index of the start point</param>
+	/// <param name="endIndex">Index of the last point</param>
+	/// <returns>A line vector in the line's local space</returns>
+	public Vector3 GetLineVector(int startIndex = 0, int endIndex = 1) => points[endIndex] - points[startIndex];
+
+	/// <summary>
+	/// Returns the direction of the line from point at <paramref name="startIndex"/>, to point at <paramref name="endIndex"/>
+	/// Returns the direction of the first line segment by default
+	/// </summary>
+	/// <param name="startIndex">Index of the start point</param>
+	/// <param name="endIndex">Index of the last point</param>
+	/// <returns>A line direction vector in the line's local space</returns>
+	public Vector3 GetDirectionVector(int startIndex = 0, int endIndex = 1) => GetLineVector(startIndex, endIndex).normalized;
+
+	public void SetPoint(int index, Vector3 position){
+		if(index > 1) throw new ArgumentException("Index > 1");
+		var oldLineVector = GetDirectionVector();
+		if(index == 0)
+			points[0] = position;
+		else
+			points[1] = position;
+		EnforceNormal(oldLineVector);
+
 	}
 
-	private void SetPointsInScene(){
-		Point firstPoint = null;
-		var totalDistance = (endPosition.transform.position - startPosition.transform.position).magnitude;
-		var pointsCount = totalDistance / intervalDistance;
-		for (int i = 0; i < pointsCount; i++) {
-			var position = Vector3.Lerp(startPosition.position, endPosition.position, i / pointsCount);
-			var newPoint = Instantiate(pointPrefab.gameObject, position, transform.rotation, pointsList)
-				.GetComponent<Point>();
-			newPoint._pointsList = pointsList;
-			if (i == 0) {
-				firstPoint = newPoint;
-			}
-			placedPoints.Add(newPoint);
-		}
-
-		if(firstPoint)
-			firstPoint._isRoot = true;
+	public void EnforceNormal(Vector3 oldLine){
+		var newLineVector = GetDirectionVector();
+		var cross = Vector3.Cross(oldLine, newLineVector);
+		var crossMagnitude = cross.magnitude;
+		var angle = Mathf.Rad2Deg * Mathf.Asin(crossMagnitude);
+		var rotation = Quaternion.AngleAxis(angle, cross);
+		normal = rotation * normal;
 	}
 
-	private void OnDrawGizmos()
-	{
-		Gizmos.color = Color.black;
-		Gizmos.DrawLine(startPosition.position, endPosition.position);
+	/// <summary>
+	/// Returns the normal of the line
+	/// </summary>
+	public Vector3 GetNormal(){
+		return normal;
+	}
 
-		var midline = Vector3.Lerp(startPosition.position, endPosition.position, 0.5f);
-		GizmosUtil.DrawArrow(midline, midline + transform.forward * normalRayLength);
+	/// <summary>
+	/// Sets the normal of the line
+	/// </summary>
+	/// <param name="norm">Normal vector of the line</param>
+	public void SetNormal(Vector3 norm){
+		normal = norm;
 	}
 }
